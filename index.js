2cb08ce6cbf3e2e20dc4e784ee9d89b87491eac8
rhw <ruohanw89@gmail.com>
2020-08-05 06:14:47 -0600

    added all the prompt questions

---
+++ index.js
@@ -0,0 +1,232 @@
+const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const { writeFile, copyFile } = require("./utils/generate-site");
+// const generatePage = require("./src/page-template");
+promptManager = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "Please enter the name of your team MANAGER.",
+        validate: (managerNameInput) => {
+          if (managerNameInput) {
+            return true;
+          } else {
+            console.log("Please enter an valiate name");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "Please enter the ID of your team MANAGER.",
+        validate: (managerIdInput) => {
+          if (managerIdInput) {
+            return true;
+          } else {
+            console.log("Please enter an valiate ID");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "Please enter the email of your team MANAGER.",
+        validate: (managerEmailInput) => {
+          if (managerEmailInput) {
+            return true;
+          } else {
+            console.log("Please enter an valiate email");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "officeNum",
+        message: "Please enter the office number.",
+        validate: (officeNumInput) => {
+          if (officeNumInput) {
+            return true;
+          } else {
+            console.log("Please enter an valiate email");
+            return false;
+          }
+        },
+      },
+    ])
+    .then(({ managerName, managerId, managerEmail, officeNum }) => {
+      this.manager = new Manager();
+      manager.name = managerName;
+      manager.id = managerId;
+      manager.email = managerEmail;
+      manager.officeNum = officeNum;
+      manager.role = "manager";
+      console.table(manager);
+      return manager;
+    })
+    .then(promptAddMember);
+};
+promptAddMember = function () {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "addMember",
+      message: "Would you like to add an EngineerOrIntern?",
+      choices: ["add Engineer", "add Intern", "No. finish building my team"],
+    })
+    .then(({ addMember }) => {
+      if (addMember === "add Engineer") {
+        promptEngineer();
+      } else if (addMember === "add Intern") {
+        promptIntern();
+      } else {
+        return;
+      }
+    });
+};
+promptEngineer = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "Please enter the name of the ENGINEER.",
+        validate: (engineerNameInput) => {
+          if (engineerNameInput) {
+            return true;
+          } else {
+            console.log("Please enter an valiate name");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "Please enter the ID of The ENGINEER.",
+        validate: (engineerIdInput) => {
+          if (engineerIdInput) {
+            return true;
+          } else {
+            console.log("Please enter an valiate ID");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "Please enter the email of The ENGINEER.",
+        validate: (engineerEmailInput) => {
+          if (engineerEmailInput) {
+            return true;
+          } else {
+            console.log("Please enter an valiate email");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "engineerGithub",
+        message: "Please enter the GitHub name of The ENGINEER.",
+        validate: (engineerGithubInput) => {
+          if (engineerGithubInput) {
+            return true;
+          } else {
+            console.log("Please enter an valiate GitHub name");
+            return false;
+          }
+        },
+      },
+    ])
+    .then(({ engineerName, engineerId, engineerEmail, engineerGithub }) => {
+      this.engineer = new Engineer();
+      engineer.name = engineerName;
+      engineer.id = engineerId;
+      engineer.email = engineerEmail;
+      engineer.github = engineerGithub;
+      engineer.role = "engineer";
+      console.table(engineer);
+      return engineer;
+    })
+    .then(promptAddMember);
+};
+promptIntern = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "internName",
+        message: "Please enter the name of the INTERN.",
+        validate: (internNameInput) => {
+          if (internNameInput) {
+            return true;
+          } else {
+            console.log("Please enter an valiate name");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "internId",
+        message: "Please enter the ID of The INTERN.",
+        validate: (internIdInput) => {
+          if (internIdInput) {
+            return true;
+          } else {
+            console.log("Please enter an valiate ID");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "internEmail",
+        message: "Please enter the email of The INTERN.",
+        validate: (internEmailInput) => {
+          if (internEmailInput) {
+            return true;
+          } else {
+            console.log("Please enter an valiate email");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "internSchool",
+        message: "Please enter the school of The INTERN.",
+        validate: (internSchoolInput) => {
+          if (internSchoolInput) {
+            return true;
+          } else {
+            console.log("Please enter an valiate email");
+            return false;
+          }
+        },
+      },
+    ])
+    .then(({ internName, internId, internEmail, internSchool }) => {
+      this.intern = new Intern();
+      intern.name = internName;
+      intern.id = internId;
+      intern.email = internEmail;
+      intern.school = internSchool;
+      intern.role = "intern";
+      console.table(intern);
+      return intern;
+    })
+    .then(promptAddMember);
+};
+
+promptManager();
+// promptEngineer();
